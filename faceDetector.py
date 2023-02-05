f0e96f8947ee83d7de37f2b72340233fd8513430
Koffi <koffi.danh@gmail.com>
2023-02-05 12:29:03 -0700

    added rectangle face coordinates

--- faceDetector.py
+++ faceDetector.py
@@ -1,8 +1,25 @@
 import cv2
-#Laoad some pre-trained data on face frontals from opencv 
+#Load some pre-trained data on face frontals from opencv 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_alt_tree.xml')
 
 # Choose an image to detect faces in
 img = cv2.imread('img\download (5).jpg')
+# Convert to grayscale
+grayScaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
+
+# Draw rectangles around the faces
+(x,y,w,h) = face_coordinates[0]
+cv2.rectangle(img, (x, y),(x+w, y+h), (0,255,0), 2)
+
+# print(face_coordinates)
+
+#
+cv2.imshow('ShowFace', img)
+cv2.waitKey()
+
+
 
 print("\nCode Completed\n")
