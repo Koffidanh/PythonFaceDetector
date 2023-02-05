b13e7b60353a7bc3f5fb331263aa1f90115b07ed
Koffi <koffi.danh@gmail.com>
2023-02-05 13:52:11 -0700

    added webcam feature

--- faceDetector.py
+++ faceDetector.py
@@ -1,25 +1,34 @@
 import cv2
-#Load some pre-trained data on face frontals from opencv 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_alt_tree.xml')
+# Load some pre-trained data on face frontals from opencv
+trained_face_data = cv2.CascadeClassifier(
+    'haarcascade_frontalface_alt_tree.xml')
 
 # Choose an image to detect faces in
-img = cv2.imread('img\download (5).jpg')
+# img = cv2.imread('img\download (5).jpg')
+
+# To capture video from webcam
+webcam = cv2.VideoCapture(0)
+
+# Iterate forever over frames
+while True:
+
+    # Read the current frame
+    successful_frame_read, frame = webcam.read()
 # Convert to grayscale
-grayScaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grayScaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 # Detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
+    face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
 
 # Draw rectangles around the faces
-(x,y,w,h) = face_coordinates[0]
-cv2.rectangle(img, (x, y),(x+w, y+h), (0,255,0), 2)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(grayScaled_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 # print(face_coordinates)
 
-#
-cv2.imshow('ShowFace', img)
-cv2.waitKey()
-
+# Display the image with the faces
+    cv2.imshow('ShowFace', grayScaled_img)
+    cv2.waitKey(1)
 
 
 print("\nCode Completed\n")
