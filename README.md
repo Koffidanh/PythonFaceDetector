d0a602d14c729e057b67162b3c7282578d142640
Koffi <koffi.danh@gmail.com>
2023-02-23 18:00:40 -0700

    read me updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PythonFaceDetector
+This Python Face Detector was created to detect frontal face.
+This Python Face Detector was create  by using an opened source "openCv". 
+
+
+
+### Resources
+* [CleverProgrammer](https://www.youtube.com/watch?v=XIrOM9oP3pA&t=8131s&ab_channel=CleverProgrammer)
+* [OpenCv](https://github.com/opencv/opencv)
+
--- faceDetector.py
+++ faceDetector.py
@@ -4,38 +4,38 @@ trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_alt_tree.xml')
 
 # Choose an image to detect faces in
-# img = cv2.imread('img\download (5).jpg')
+img = cv2.imread('img\images (7).jpg')
 
 # To capture video from webcam
-webcam = cv2.VideoCapture(0)
+# webcam = cv2.VideoCapture(0)
 
 # Iterate forever over frames
-while True:
+# while True:
 
-    # Read the current frame
-    successful_frame_read, frame = webcam.read()
+# Read the current frame
+# successful_frame_read, frame = webcam.read()
 # Convert to grayscale
-    grayScaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+grayScaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Detect faces
-    face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
+face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
 
 # Draw rectangles around the faces
-    for (x, y, w, h) in face_coordinates:
-        cv2.rectangle(grayScaled_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(grayScaled_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 # print(face_coordinates)
 
 # Display the image with the faces
-    cv2.imshow('ShowFace', grayScaled_img)
-    key = cv2.waitKey(1)
+cv2.imshow('ShowFace', grayScaled_img)
+key = cv2.waitKey()
 
 # Stop if Q key is pressed
-    if key == 81 or key == 113:
-        break
+# if key == 81 or key == 113:
+# break
 
 # Release the videoCapture object
 
-webcam.release()
+# webcam.release()
 
 print("\nCode Completed\n")
