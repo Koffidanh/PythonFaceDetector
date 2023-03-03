a4497923db52004db79bc48f6b4c80a6fcd6ae1f
Koffi <koffi.danh@gmail.com>
2023-03-02 18:56:41 -0700

    updating the stwich statement

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # PythonFaceDetector
 This Python Face Detector was created to detect frontal face.
-This Python Face Detector was create  by using an opened source "openCv". 
+This Python Face Detector was created  by using an opened source "openCv".
+To use The PythonFaceDetector, the user need to fork the repo and run the code in python environment. The application will add to put img for image or vid for video. 
 
 
 
--- faceDetector.py
+++ faceDetector.py
@@ -3,39 +3,66 @@ import cv2
 trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_alt_tree.xml')
 
-# Choose an image to detect faces in
-img = cv2.imread('img\images (7).jpg')
+# input to pick image or video
 
-# To capture video from webcam
-# webcam = cv2.VideoCapture(0)
+ans = input(
+    "Do you want to try the image or video detector ? (type img for image or vid for video)")
 
-# Iterate forever over frames
-# while True:
+# create a if conditional to switch between image or video
 
-# Read the current frame
-# successful_frame_read, frame = webcam.read()
-# Convert to grayscale
-grayScaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if ans == "image" or "img":
+    print("image")
+    print(ans)
+    # Choose an image to detect faces in
+    img = cv2.imread('img\download (5).jpg')
 
-# Detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
+    # Convert to grayscale
+    grayScaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Draw rectangles around the faces
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(grayScaled_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
 
-# print(face_coordinates)
+    # Draw rectangles around the faces
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(grayScaled_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-# Display the image with the faces
-cv2.imshow('ShowFace', grayScaled_img)
-key = cv2.waitKey()
+    # Display the image with the faces
+    cv2.imshow('ShowFace', grayScaled_img)
+    key = cv2.waitKey()
+
+elif ans == "video" or "vid":
+    print("video")
+    print(ans)
+    # To capture video from webcam
+    webcam = cv2.VideoCapture(0)
+
+    # Iterate forever over frames
+    while True:
+
+        # Read the current frame
+        successful_frame_read, frame = webcam.read()
+    # Convert to grayscale
+        grayScaled_vid = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+        face_coordinates = trained_face_data.detectMultiScale(grayScaled_vid)
+
+    # Draw rectangles around the faces
+        for (x, y, w, h) in face_coordinates:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # print(face_coordinates)
+
+    # Display the image with the faces
+        cv2.imshow('ShowFace', frame)
+        key = cv2.waitKey(1)
 
 # Stop if Q key is pressed
 # if key == 81 or key == 113:
-# break
+    # break
 
-# Release the videoCapture object
+    # Release the videoCapture object
 
-# webcam.release()
-
-print("\nCode Completed\n")
+    # webcam.release()
+else:
+    print("\nCode Completed\n")
